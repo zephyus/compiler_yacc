77c47f88a5fc13016f5ef1f1e656ed442a0e1d8d
zephyus <113088208+zephyus@users.noreply.github.com>
2025-06-04 10:40:54 +0800

    Create test6.java

---
+++ test6.java
@@ -0,0 +1,25 @@
+class test6{
+    void sum(){
+//------NEVER USED
+        int sumxyz = x + y + z ;
+    }
+    void main() {
+//------ARRAY
+        int [] i= new int [1];
+        for(i[0] = 0; i[0]<5; i[0]++)
+            i[0]++;
+
+//------NEW CLASS
+        Point lowerLeft = new Point() ;
+
+//------ERROR CONDITION
+        while(**/a++){
+            print("error!!");
+        }
+//------CLASS DECLARE
+        class Point {
+            int x, y, z;
+        }
+    }
+
+}
