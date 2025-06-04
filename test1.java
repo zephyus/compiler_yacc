aa8ddc094740c410d695691a353a5ad892ce36b4
zephyus <113088208+zephyus@users.noreply.github.com>
2025-06-04 10:39:14 +0800

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,20 @@
+/* Test file: Perfect test file
+ * Compute sum = 1 + 2 + ... + n
+ */
+class sigma {
+  // "final" should have const_expr
+  final int n = 10;
+  int sum, index;
+
+  main()
+  {
+    index = 0;
+    sum = 0;
+    while (index <= n)
+    {
+      sum = sum + index;
+      index = index + 1;
+    }
+    print(sum);
+  }
+}
